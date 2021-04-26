53c740f627e88040327648a3314b7a277d8b9140
ferpioli <ferpiolli@gmail.com>
2021-04-26 19:14:20 -0300

    pesquisa de livros e pesquisa de livros por ID

--- routes/livrosRoute.js
+++ routes/livrosRoute.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 
 const DB_URL = "mongodb+srv://admin:admin@cluster0.chi5w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true });
 
 const db = mongoose.connection;
 
@@ -27,37 +27,48 @@ const router = express.Router();
 let livrosArray = livros;
 
 router.get("/", (req, res) => {
-    res.json(livros);
+    livrosModel.find((err, livro) => {
+        if (err) res.status(500).send(err);
+        res.json(livro);
+    });
 });
 
 router.get("/:id", (req, res) => {
-    const livro = livros.find(value => value.id == req.params.id);
-
-    if (livro) {
-        res.json(livro);
-    } else {
-        res.send('livro não encontrado');
-    }
-    res.end();
+    /*  const livro = livros.find(value => value.id == req.params.id);
+  
+      if (livro) {
+          res.json(livro);
+      } else {
+          res.send('livro não encontrado');
+      }
+      res.end();*/
+    livrosModel.findById(req.params.id, (err, livro) => {
+        if (livro) {
+            res.json(livro);
+        } else {
+            res.status(404).send(`livro com id ${req.params.id} não encontrado`);
+        }
+        res.end();
+    })
 });
 
 
 router.post("/", (req, res) => {
-   /* console.log(req.body);
-    livrosArray.push(req.body);
-    res.status(200).send("Ok");
-    res.end();*/
-    const id = new  mongoose.Types.ObjectId();
+    /* console.log(req.body);
+     livrosArray.push(req.body);
+     res.status(200).send("Ok");
+     res.end();*/
+    const id = new mongoose.Types.ObjectId();
     const livroParaSalvar = Object.assign({
         _id: id
     }, req.body);
     //console.log(JSON.stringify(livroParaSalvar));
     const livro = new livrosModel(livroParaSalvar);
-    livro.save().then((err, livro)=> {
-        if(err) res.status(500).send(err);
+    livro.save().then((err, livro) => {
+        if (err) res.status(500).send(err);
         res.json(livro);
     })
-}); 
+});
 
 
 router.put("/", (req, res) => {
@@ -70,12 +81,12 @@ router.delete("/", (req, res) => {
     res.end();
 });
 
-router.param("id", (req, res, next, id) => {
+/*router.param("id", (req, res, next, id) => {
     if (isNaN(id)) {
         next("não é numero");
     } else {
         next();
     }
 
-});
+});*/
 export default router;
