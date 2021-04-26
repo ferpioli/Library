61555122675f3aa41e97b1de9f4052615b6f8175
ferpioli <ferpiolli@gmail.com>
2021-04-26 17:57:35 -0300

    inclusão da rota post no mongoDB

--- routes/livrosRoute.js
+++ routes/livrosRoute.js
@@ -1,6 +1,26 @@
 import express from "express";
 import livros from "../data/livros.json";
+import mongoose from "mongoose";
 
+
+
+const DB_URL = "mongodb+srv://admin:admin@cluster0.chi5w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true});
+
+const db = mongoose.connection;
+
+db.once("", () => {
+    console.log("Estanmos conectados ao MongoDB")
+});
+
+const livrosSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    titulo: String,
+    tipo: String
+});
+
+const livrosModel = mongoose.model("livros", livrosSchema);
 
 const router = express.Router();
 
@@ -11,23 +31,33 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-   const livro = livros.find(value => value.id == req.params.id);
+    const livro = livros.find(value => value.id == req.params.id);
 
-   if (livro) {
-       res.json(livro);
-  } else {
-      res.send('livro não encontrado');
-  }
-  res.end();
+    if (livro) {
+        res.json(livro);
+    } else {
+        res.send('livro não encontrado');
+    }
+    res.end();
 });
 
 
 router.post("/", (req, res) => {
-    console.log(req.body);
+   /* console.log(req.body);
     livrosArray.push(req.body);
     res.status(200).send("Ok");
-    res.end();
-});
+    res.end();*/
+    const id = new  mongoose.Types.ObjectId();
+    const livroParaSalvar = Object.assign({
+        _id: id
+    }, req.body);
+    //console.log(JSON.stringify(livroParaSalvar));
+    const livro = new livrosModel(livroParaSalvar);
+    livro.save().then((err, livro)=> {
+        if(err) res.status(500).send(err);
+        res.json(livro);
+    })
+}); 
 
 
 router.put("/", (req, res) => {
@@ -41,11 +71,11 @@ router.delete("/", (req, res) => {
 });
 
 router.param("id", (req, res, next, id) => {
-    if (isNaN(id)){
+    if (isNaN(id)) {
         next("não é numero");
-    }else{
+    } else {
         next();
     }
-    
+
 });
 export default router;
